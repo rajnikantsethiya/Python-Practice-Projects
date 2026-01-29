31509e0a543c812bb203d0c8a80b8cc944e3d59e
rajnikantsethiya <rajnikant04sethiya@gmail.com>
2026-01-29 17:40:18 +0530

    Added own exception error and handled multiple exceptions

--- Practice/Snippets/ExceptionHandling.py
+++ Practice/Snippets/ExceptionHandling.py
@@ -9,3 +9,28 @@ def Student(age):
         print("Student registered")
     
 Student(11)
+
+# Creating own exception error
+class EmployeeError(Exception): pass
+class Office(Exception):
+    def __init__(self, employee, *args):
+        super().__init__(*args)
+        self.employee = employee
+    
+    def Play(self):
+        try:
+            if not isinstance(self.employee, str):
+                raise ValueError("Please provide the employee name correctly !")
+            elif self.employee in ["Raj", "Rishi", "Prashant"]:
+                print("Play carrom !", type(self.employee))
+            else:
+                raise EmployeeError("This employee is not allowed to play !")
+        except Exception as e:
+            print("Error", e)
+        finally:
+            print("Thanks for playing !")
+
+ofc = Office("Rauhul")
+ofc.Play()
+ofc = Office(123)
+ofc.Play()
